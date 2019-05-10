6bb6d080dce91002925a8fa9d15d6d41ba3c8dde
ITITIU17065 <49957706+ITITIU17065@users.noreply.github.com>
2019-05-11 00:44:40 +0700

    Update Board

--- 2048/src/com/thanh/game/Board.java
+++ 2048/src/com/thanh/game/Board.java
@@ -1,20 +1,13 @@
 package com.thanh.game;
 
-import java.awt.*;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Board extends Canvas {
 	public void paint(Graphics g) {
 		setBackground(Color.decode("#FAF8EF"));
 		g.setColor(Color.decode("#BBADA0"));
 		g.fillRoundRect(50, 50, 400, 400, 10, 10);
-		// draw grid
-
-		g.setColor(Color.decode("#CDC1B4"));
-		for (int i = 70; i < 400; i += 95) {
-			for (int j = 70; j < 400; j += 95) {
-				g.fillRoundRect(j, i, 75, 75, 10, 10);
-			}
-		}
-
 	}
 }
