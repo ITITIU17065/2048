2e1eec0d406bacd7600ddce695902ad3e1da16ea
ITITIU17065 <49957706+ITITIU17065@users.noreply.github.com>
2019-05-24 23:52:33 +0700

    make Window again

--- 2048/src/com/thanh/game/Game.java
+++ 2048/src/com/thanh/game/Game.java
@@ -1,47 +1,57 @@
 package com.thanh.game;
 
-public class Game implements Runnable {
-	public int width, height;
-	public String title;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageFilter;
 
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements KeyListener, Runnable {
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
+	public static final Font main = new Font("Clear Sans", Font.BOLD, 30);
+	private Thread game;
 	private boolean running = false;
-	private Thread thread;
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-	public Game(String title, int width, int height) {
-		this.width = width;
-		this.height = height;
-		this.title = title;
-	}
-
-	private void init() {
-		new Display(title, width, height);
+	public Game() {
+		setFocusable(true);
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		addKeyListener(this);
 	}
 
 	private void update() {
+
 	}
 
 	private void render() {
+		Graphics2D g = (Graphics2D) image.getGraphics();
+		g.setColor(Color.decode("#FAF8EF"));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.decode("#BBADA0"));
+		g.fillRoundRect(50, 50, 400, 400, 10, 10);
+		// Draw the board
+		g.dispose();
 
-	}
-
-	public void run() {
-
-		init();
-
-		while (running) {
-			update();
-			render();
-		}
-		stop();
-
+		Graphics2D g2d = (Graphics2D) getGraphics();
+		g2d.drawImage(image, 0, 0, null);
+		g2d.dispose();
 	}
 
 	public synchronized void start() {
 		if (running)
 			return;
 		running = true;
-		thread = new Thread(this);
-		thread.start();
+		game = new Thread(this);
+		game.start();
 	}
 
 	public synchronized void stop() {
@@ -49,10 +59,29 @@ public class Game implements Runnable {
 			return;
 		running = false;
 		try {
-			thread.join();
+			game.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
+	@Override
+	public void run() {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
 }
