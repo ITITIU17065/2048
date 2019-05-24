e32194fa76040c1564d7166d9af02580b4d146ac
ITITIU17065 <49957706+ITITIU17065@users.noreply.github.com>
2019-05-24 18:28:43 +0700

    add Game loop

--- 2048/src/com/thanh/game/Game.java
+++ 2048/src/com/thanh/game/Game.java
@@ -1,44 +1,51 @@
 package com.thanh.game;
 
-public class Game  implements Runnable{
-	private Board board;
-	
+public class Game implements Runnable {
+	public int width, height;
+	public String title;
+
 	private boolean running = false;
 	private Thread thread;
-	
-	private void init(){
-		board = new Board();
+
+	public Game(String title, int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.title = title;
 	}
-	
+
+	private void init() {
+		new Display(title, width, height);
+	}
+
 	private void update() {
-		board.paint();
 	}
-	
+
 	private void render() {
-		
+
 	}
-	public void run(){
-		
+
+	public void run() {
+
 		init();
-		
-		while(running){
+
+		while (running) {
 			update();
-			
+			render();
 		}
 		stop();
-		
+
 	}
-	
-	public synchronized void start(){
-		if(running)
+
+	public synchronized void start() {
+		if (running)
 			return;
 		running = true;
 		thread = new Thread(this);
 		thread.start();
 	}
-	
-	public synchronized void stop(){
-		if(!running)
+
+	public synchronized void stop() {
+		if (!running)
 			return;
 		running = false;
 		try {
@@ -47,5 +54,5 @@ public class Game  implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- 2048/src/com/thanh/game/Start.java
+++ 2048/src/com/thanh/game/Start.java
@@ -3,7 +3,7 @@ package com.thanh.game;
 public class Start {
 	
 	public static void main(String[] args) {
-		Game game = new Game();
+		Game game = new Game("2048", 500, 500);
 		game.start();
 	}
 
