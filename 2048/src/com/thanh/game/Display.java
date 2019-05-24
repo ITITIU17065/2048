043410ee979c7bd627e4ae7ef2d857d4ff80c200
ITITIU17065 <49957706+ITITIU17065@users.noreply.github.com>
2019-05-24 18:16:49 +0700

    add Canvas to Display class

--- 2048/src/com/thanh/game/Display.java
+++ 2048/src/com/thanh/game/Display.java
@@ -1,9 +1,13 @@
 package com.thanh.game;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Display {
 	private JFrame frame;
+	private Canvas canvas;
 
 	private String title;
 	private int width, height;
@@ -23,5 +27,13 @@ public class Display {
 		frame.setSize(width, height);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+		
+		frame.add(canvas);
+		frame.pack();
 	}
 }
